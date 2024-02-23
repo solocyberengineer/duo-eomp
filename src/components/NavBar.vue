e89a4bdec6e34b70f628249f0986dad57d5e007d
GSamuels5 <ghamzahsamuels23@gmail.com>
2024-02-23 15:46:09 +0200

    navbar added

---
+++ src/components/NavBar.vue
@@ -0,0 +1,43 @@
+<template>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <router-link to="/" class="nav-link active" aria-current="page" href="#">Home</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="/products" class="nav-link" href="#">Products</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="/product" class="nav-link" href="#">Product</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="/admin" class="nav-link" href="#">Admin</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="/contact" class="nav-link" href="#">Contact us</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="/login" class="nav-link" href="#">Login</router-link>
+        </li>
+    
+      </ul>
+    </div>
+  </div>
+</nav>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,19 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<div class="container fluid">
+<div>
+  <h2>Home</h2>
+</div>
+</div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+  
   }
 }
 </script>
