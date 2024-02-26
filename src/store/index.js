c2b6cc4e24d670dc506e3fcc4b1bfbfa5e62598e
GSamuels5 <ghamzahsamuels23@gmail.com>
2024-02-26 09:52:58 +0200

    render link added

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,11 @@
 import { createStore } from 'vuex'
-
+import sweet from 'sweetalert'
+import {useCookies} from 'vue3-cookies'
+import AuthenticateUser from '../service/Authentication.js'
+const liveUrl = 'https://duo-eomp-gs-ro.onrender.com/'
 export default createStore({
   state: {
+    
   },
   getters: {
   },
