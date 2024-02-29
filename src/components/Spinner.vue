a85c0e9752b42fd786046ef99989688e8a428f07
GSamuels5 <ghamzahsamuels23@gmail.com>
2024-02-29 16:00:24 +0200

    spinner added

---
+++ src/components/Spinner.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <div class="d-flex justify-content-center">
+  <div class="spinner-border" role="status">
+    <span class="visually-hidden">Loading...</span>
+  </div>
+</div>
+    </div>
+</template>
+
+<script>
+    export default {
+       name: 'spinnerComp' 
+    }
+</script>
+
+<style  scoped>
+
+</style>
